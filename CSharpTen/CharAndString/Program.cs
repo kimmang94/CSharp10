3a0865986433189faf1aa17e366239ecba89d9dd
Kimhyeok <kimmang940715@naver.com>
2024-07-23 22:48:23 +0900

    문자와 문자열

---
+++ CSharpTen/CharAndString/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CharAndString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello";
+            string b = "h";
+
+            b = b + "ello";
+            Console.WriteLine(a == b);
+            Console.WriteLine("b = " + b);
+
+            int x = 10;
+            string c = b + '!' + x;
+            Console.WriteLine("c = " + c);
+        }
+    }
+}
