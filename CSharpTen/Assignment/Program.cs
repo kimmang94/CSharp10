acab7fde9ac3158b9263256c0822f9cd7b61fc86
Kimhyeok <kimmang940715@naver.com>
2024-07-24 22:28:02 +0900

    대입연산자와 대입

---
+++ CSharpTen/Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double x;
+
+            i = 5;
+            x = 3.141592;
+            Console.WriteLine("i = " + i + ", x = " + x);
+
+            x = i;
+            i = (int)x;
+            Console.WriteLine("i = " + i + ", x = " + x);
+
+        }
+    }
+}
